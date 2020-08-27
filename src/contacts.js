78f96dc1e09a3091fffac361bfd59e1df7464eb6
ictrl <49734163+gauravkumar34@users.noreply.github.com>
2020-08-27 17:59:24 +0530

    v 0.0.1

---
+++ src/contacts.js
@@ -0,0 +1,30 @@
+const contacts = [
+  {
+    name: "Beyounce",
+    img: "https://data.whicdn.com/images/222445623/original.jpg",
+    tel: "+91-909909",
+    email: "b@beyounce.com",
+  },
+  {
+    name: "Gaurav Kumar",
+    img: "https://gauravkumar34.github.io/portfolio/images/about-1.jpg",
+    tel: "+91 7011267344",
+    email: "urwid@gauav.com",
+  },
+  {
+    name: "Jack Bauer",
+    img:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    tel: "+987 654 321",
+    email: "jack@nowhere.com",
+  },
+  {
+    name: "Chuck Norris",
+    img:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    tel: "+918 372 574",
+    email: "gmail@chucknorris.com",
+  },
+];
+
+export default contacts;
